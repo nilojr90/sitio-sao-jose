95e930b126aed785dde54906dc245dcfdcdd794b
Nilson Ramos <nilojr90@gmail.com>
2021-09-27 12:35:39 -0300

    novo background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement } from 'react';
 import './App.scss';
-import backgroundH from '../assets/background.png';
-import backgroundV from '../assets/background-v.png';
+import SocialBar from 'components/SocialBar';
+import backgroundH from '../assets/background.jpg';
+import backgroundV from '../assets/background-v.jpg';
 
 function App(): ReactElement {
   return (
     <div className="App">
       <img className="only-desktop" src={backgroundH} alt="" />
       <img className="only-mobile" src={backgroundV} alt="" />
+      <SocialBar />
     </div>
   );
 }
